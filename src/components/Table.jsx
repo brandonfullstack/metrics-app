3587b6418c575de000127b252a3f6c28ebc54137
Brandon Wright <brandonfullstack@gmail.com>
2024-03-08 14:40:54 -0500

    Allow post functionality

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PORTALS from "../constants/portals";
 import INTERACTIONS from "../constants/interactions";
 import { usePortals } from "../hooks/usePortals";
@@ -6,12 +6,25 @@ import { usePortals } from "../hooks/usePortals";
 export default function Table() {
 
   const { portals } = usePortals();
-  const [metrics, setMetrics] = React.useState({});
+  const [metric, setMetric] = React.useState();
 
   const handleMetricInput = (metric) => {
-    const metricObject = { value: metric.value, portalId: metric.portal, interactionId: metric.interaction, date: new Date()};
+    const metricObject = { value: parseInt(metric.value), portalId: metric.portal, interactionId: metric.interaction, date: new Date() };
 
     console.log(metricObject);
+
+    if (!metric.value) {
+      return;
+    }
+    const response = fetch("http://localhost:5000/api/metrics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(metricObject)
+    }).catch(function(error){
+      console.log('Request failed', error);
+    })
   }
 
   return (
@@ -31,7 +44,7 @@ export default function Table() {
               <th className="w-10 text-end align-middle">{interaction.name}</th>
               {portals.map((portal, index) => (
                 <td key={index} className="text-nowrap">
-                  <input type="number" min="0" className="form-control" id={`${portal.id + "_" + interaction.id}`} name={`${portal.id + "_" + interaction.id}`} tabIndex={`${index + 1}`} onChange={(e) => handleMetricInput({ value: e.target.value, portal: portal.id, interaction: interaction.id })}/>
+                  <input type="number" min="0" className="form-control" id={`${portal.id + "_" + interaction.id}`} name={`${portal.id + "_" + interaction.id}`} tabIndex={`${index + 1}`} onBlur={(e) => handleMetricInput({ value: e.target.value, portal: portal.id, interaction: interaction.id })} />
                 </td>
               ))}
             </tr>
--- src/constants/interactions.js
+++ src/constants/interactions.js
@@ -1,9 +1,9 @@
 export const INTERACTIONS = [
-  { name: "Calls Made", id: "calls-made" },
-  { name: "Calls Received", id: "calls-received" },
-  { name: "Emails Received", id: "emails-received" },
-  { name: "Emails Sent", id: "emails-sent" },
-  { name: "Meetings", id: "meetings" },
+  { name: "Calls Made", id: 1 },
+  { name: "Calls Received", id: 2 },
+  { name: "Emails Received", id: 3 },
+  { name: "Emails Sent", id: 4 },
+  { name: "Meetings", id: 5 },
   { name: "Support Tickets Assigned", id: "support-tickets-assigned" },
   { name: "Support Tickets Completed", id: "support-tickets-completed" },
   { name: "Voicemails", id: "voicemails" }
