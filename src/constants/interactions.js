90ddf23de3cd0a768b81e2c23b8dc6bbcf75fc4b
Brandon Wright <brandonfullstack@gmail.com>
2024-03-10 00:18:23 -0500

    Update constants

--- src/constants/interactions.js
+++ src/constants/interactions.js
@@ -4,9 +4,9 @@ export const INTERACTIONS = [
   { name: "Emails Received", id: 3 },
   { name: "Emails Sent", id: 4 },
   { name: "Meetings", id: 5 },
-  { name: "Support Tickets Assigned", id: "support-tickets-assigned" },
-  { name: "Support Tickets Completed", id: "support-tickets-completed" },
-  { name: "Voicemails", id: "voicemails" }
+  { name: "Support Tickets Assigned", id: 6 },
+  { name: "Support Tickets Completed", id: 7 },
+  { name: "Voicemails", id: 8 }
 ]
 
 export default INTERACTIONS;
--- src/constants/portals.js
+++ src/constants/portals.js
@@ -4,12 +4,12 @@ export const PORTALS = [
   { name: "CTLS", id: 3 },
   { name: "HIVD2C", id: 4 },
   { name: "STARS", id: 5 },
-  { name: "PCEligibilty", id: "pceligibility" },
-  { name: "TOPWA", id: "topwa" },
-  { name: "ELR", id: "elr" },
-  { name: "PCFMRS", id: "pcfmrs" },
-  { name: "MOVEit", id: "moveit" },
-  { name: "Prenatal", id: "prenatal"}
+  { name: "PCEligibilty", id: 6 },
+  { name: "TOPWA", id: 7 },
+  { name: "ELR", id: 8 },
+  { name: "PCFMRS", id: 9 },
+  { name: "MOVEit", id: 10 },
+  { name: "Prenatal", id: 11 }
 ]
 
 export default PORTALS;
