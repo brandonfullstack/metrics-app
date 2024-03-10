d0fbb279ca2ab2fc9424052d49ff0c6a614d135a
Brandon Wright <brandonfullstack@gmail.com>
2024-03-10 15:24:11 -0400

    Make brand reload page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
 
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">metricsapp</a>
+        <a className="navbar-brand" href="/">metricsapp</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
