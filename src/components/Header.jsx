609ac38cb4da8c4c180640d842b911ca5679dce6
Brandon Wright <brandonfullstack@gmail.com>
2024-03-10 00:18:47 -0500

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
 
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">metricsapp</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -20,7 +20,7 @@ function Header() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
+                Brandon Wright
               </a>
               <ul className="dropdown-menu dropdown-menu-end">
               <li><a className="dropdown-item" href="#">Profile</a></li>
